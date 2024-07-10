fa00695f253ff600c11525e65f62eee420a7f183
clintonthedeveloper <ochiengclinton95@gmail'com>
2024-07-10 12:09:28 +0300

    initial commit

---
+++ myeshop/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyeshopConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'myeshop'
---
+++ myeshop/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def services(request):
+    return render(request, 'services.html')
